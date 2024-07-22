b5dfc45ac6fa87aa4f098c603a9439f26ee1bbc9
McTintor <igortintor99@gmail.com>
2024-07-22 17:08:44 +0200

    implemented api and made app logic

--- src/app.js
+++ src/app.js
@@ -1,1 +1,54 @@
-import "./styles.css"
+import './styles.css'
+
+async function getData(city) {
+	const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'${city}'?unitGroup=metric&key=WR73STJT6RY7U9ZZ8D5VQBVGV&contentType=json`
+	try {
+		const response = await fetch(url)
+		if (!response.ok) {
+			alert('Please provide a valid location')
+			return
+		}
+
+		const data = await response.json()
+		console.log(data)
+		return data
+	} catch (error) {
+		console.error(error.message)
+		throw error
+	}
+}
+
+function updateUI(data) {
+	const location = document.querySelector('#location')
+	const time = document.querySelector('#time')
+	const conditions = document.querySelector('#conditions')
+	const temp = document.querySelector('#temp')
+	const feelsLike = document.querySelector('#feels-like')
+	const description = document.querySelector('#description')
+
+	location.innerHTML = `Location: ${data.resolvedAddress}`
+	time.innerHTML = `Time: ${data.currentConditions.datetime}`
+	conditions.innerHTML = `Current Conditions: ${data.currentConditions.conditions}`
+	temp.innerHTML = `Current Temperature: ${data.currentConditions.temp}`
+	feelsLike.innerHTML = `Feels Like: ${data.currentConditions.feelslike}`
+	description.innerHTML = data.description
+}
+
+async function handleSearch() {
+	try {
+		const city = document.querySelector('#city').value
+		const data = await getData(city)
+		updateUI(data)
+	} catch (error) {
+		console.error('Error in handleSearch: ', error)
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	const cityInput = document.querySelector('#city')
+	const submitElement = document.querySelector('#submit')
+	submitElement.addEventListener('click', () => {
+		handleSearch()
+		cityInput.value = ''
+	})
+})
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,28 +1,23 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require('path')
+const {merge} = require('webpack-merge')
+const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ],
-      },
-    ],
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  output: {
-    filename: "[name].bundle.js", 
-    path: path.resolve(__dirname, "dist"), 
-    assetModuleFilename: "assets/[name][ext]",
-  },
-});
+	mode: 'development',
+	module: {
+		rules: [
+			{test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
+			{test: /\.css$/, use: ['style-loader', 'css-loader']},
+		],
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		static: './dist',
+		hot: true,
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		assetModuleFilename: 'assets/[name][ext]',
+	},
+})
